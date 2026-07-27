68e1c0368fa1d9269b15388ff8e5c3cb4d9dd1bf
Improver <95467688+ImproverX@users.noreply.github.com>
2026-07-27 12:24:12 +0300

    Create main.asm

---
+++ SRC/main.asm
@@ -0,0 +1,154 @@
+L_MON:	.EQU	08000h+04000h
+	.ORG	00100h
+START:	DI
+	XRA  A
+	OUT	010h
+	OUT	011h
+	MVI  A, 033h	; ОЗУ: Банк 0, Банк 2
+	OUT     00Eh	; режим ОЗУ
+	LXI SP,	08000h
+	LXI  H, L_PAK
+	LXI  D, L_MON
+	CALL	unlzsa1	; распаковка
+	JMP	L_MON
+;
+	.db "-= (c) Ларин "
+	.db "Юрий aka IMPROVER"
+	.db " =- 27/07/2026 "
+;
+;input: 	hl=compressed data start
+;		de=uncompressed destination start
+unlzsa1:
+	mvi b,0
+	jmp ReadToken
+;
+NoLiterals:
+	xra m
+	push d
+	inx h
+	mov e,m
+	jm LongOffset
+ShortOffset:
+	mvi d,0FFh
+	adi 3
+	cpi 15+3
+	jnc LongerMatch
+CopyMatch:
+	mov c,a
+CopyMatch_UseC:
+	inx h
+	xthl
+	xchg
+	dad d
+	mov a,m
+	inx h
+	stax d
+	inx d
+	dcx b
+	mov a,m
+	inx h
+	stax d
+	inx d
+	dcx b
+	dcx b
+	inr c
+BLOCKCOPY1:
+	mov a,m
+	stax d
+	inx h
+	inx d
+	dcr c
+	jnz BLOCKCOPY1
+	xra a
+	ora b
+	jz $+7
+	dcr b
+	jmp BLOCKCOPY1
+	pop h
+ReadToken:
+	mov a,m
+	ani 70h
+	jz NoLiterals 
+	cpi 70h
+	jz MoreLiterals
+	rrc
+	rrc
+	rrc
+	rrc
+	mov c,a
+	mov b,m
+	inx h
+	mov a,m		; <<<
+	stax d
+	inx h
+	inx d
+	dcr c
+	jnz $-5		; >>>
+	push d
+	mov e,m
+	mvi a,8Fh
+	ana b
+	mvi b,0
+	jp ShortOffset
+LongOffset:
+	inx h
+	mov d,m
+	adi -128+3
+	cpi 15+3
+	jc CopyMatch
+LongerMatch:
+	inx h
+	add m
+	jnc CopyMatch
+	mov b,a
+	inx h
+	mov c,m
+	jnz CopyMatch_UseC
+	inx h
+	mov b,m
+	mov a,b
+	ora c
+	jnz CopyMatch_UseC
+	pop d
+	ret
+;
+MoreLiterals:		
+	xra m
+	push psw
+	mvi a,7
+	inx h
+	add m
+	jc ManyLiterals
+CopyLiterals:
+	mov c,a
+CopyLiterals_UseC:
+	inx h
+	dcx b
+	inr c
+BLOCKCOPY2:
+	mov a,m
+	stax d
+	inx h
+	inx d
+	dcr c
+	jnz BLOCKCOPY2
+	xra a
+	ora b
+	jz $+7
+	dcr b
+	jmp BLOCKCOPY2
+	pop psw
+	push d
+	mov e,m
+	jp ShortOffset
+	jmp LongOffset
+ManyLiterals:
+	mov b,a
+	inx h
+	mov c,m
+	jnz CopyLiterals_UseC
+	inx h
+	mov b,m
+	jmp CopyLiterals_UseC	
+L_PAK:	.end
+	
