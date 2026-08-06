29bb8be32d671831791acce44e537f6e36078c3d
Improver <95467688+ImproverX@users.noreply.github.com>
2026-08-06 11:11:48 +0300

    Update main.asm

--- SRC/main.asm
+++ SRC/main.asm
@@ -14,7 +14,7 @@ START:	DI
 ;
 	.db "-= (c) Ларин "
 	.db "Юрий aka IMPROVER"
-	.db " =- 05/08/2026 "
+	.db " =- 06/08/2026 "
 ;
 ;input: 	hl=compressed data start
 ;		de=uncompressed destination start
